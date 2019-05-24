7b4b0d985ad6b9cbbb6883992c52ba28cf2c3c04
fuachk <49823658+fuachk@users.noreply.github.com>
2019-05-24 23:15:53 +0800

    Create 留言板代码 class1.php

---
+++ 留言板代码 class1.php
@@ -0,0 +1,35 @@
+<style>
+
+    .bord{ width:300px; height:50px; border:1px solid #000; text-align:center}
+    .addition{width: 90% ;  text-align:center}
+    .title1{align="right"; style="border:1px }
+    .title2{align = "right" ; valign = "top"}
+    .submit{ colspan="2" ; text-align="center" }
+    .center {
+        position: relative;
+        height: 500px;}
+    .center_text{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 300px;
+        height: 600px;
+    }
+</style>
+<div class="bord">我被居中显示</div>
+<div class="addition">添加留言</div>
+<div class="title1">标题</div>
+<div class="title2">123</div>
+<div class="submit">123</div>
+<div class="center">
+    　　<div class="center_text">
+        　　　　我是要居中的内容<br>　　</div> <br></div>
+
+
+<!--
+<html> 与</html> 之间的文本描述网页
+<body> 与</body> 之间的文本是可见的页面内容
+<h1> 与 </h1> 之间的文本被显示为标题
+<p> 与 </p> 之间的文本被显示为段落
+-->
